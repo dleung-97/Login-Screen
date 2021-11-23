86e5f35793988ba98c7e241e379eb1dac1fad13b
dleung-wsu <94886695+dleung-wsu@users.noreply.github.com>
2021-11-22 23:30:59 -0600

    Add files via upload

---
+++ Login Screen/Login Screen/Login_ScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_ScreenApp.swift
+//  Login Screen
+//
+//  Created by David L on 11/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Login_ScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
